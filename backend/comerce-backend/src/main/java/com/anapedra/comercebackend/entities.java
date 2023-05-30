e2533d3ecfefbd5edabf1b267a0136b3455bd6be
Ana Santana <anapedra.mil@gmail.com>
2023-05-30 17:45:14 -0300

    Product,category,properties

--- backend/comerce-backend/src/main/java/com/anapedra/comercebackend/entities.java
+++
@@ -1,4 +0,0 @@
-package com.anapedra.comercebackend;
-
-public class entities {
-}
---
+++ backend/comerce-backend/src/main/java/com/anapedra/comercebackend/entities/Product.java
@@ -0,0 +1,133 @@
+package com.anapedra.comercebackend.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String shortDescription;
+    private String fullDescription;
+    private Instant momentRegistration;
+    private Instant momentUpdate;
+    private Double productCost;
+    private Double initialPrice;
+    private String imgUrl;
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",joinColumns =
+    @JoinColumn(name = "product_id"),inverseJoinColumns =
+    @JoinColumn(name = "category_id"))
+    private Set<Category>categories=new HashSet<>();
+
+    public Product(Long id, String shortDescription, String fullDescription, Instant momentRegistration,
+                   Instant momentUpdate, Double productCost, Double initialPrice, String imgUrl) {
+        this.id = id;
+        this.shortDescription = shortDescription;
+        this.fullDescription = fullDescription;
+        this.momentRegistration = momentRegistration;
+        this.momentUpdate = momentUpdate;
+        this.productCost = productCost;
+        this.initialPrice = initialPrice;
+        this.imgUrl = imgUrl;
+    }
+
+    public Product() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getFullDescription() {
+        return fullDescription;
+    }
+
+    public void setFullDescription(String fullDescription) {
+        this.fullDescription = fullDescription;
+    }
+
+    public Instant getMomentRegistration() {
+        return momentRegistration;
+    }
+
+    public void setMomentRegistration(Instant momentRegistration) {
+        this.momentRegistration = momentRegistration;
+    }
+
+    public Instant getMomentUpdate() {
+        return momentUpdate;
+    }
+
+    public void setMomentUpdate(Instant momentUpdate) {
+        this.momentUpdate = momentUpdate;
+    }
+
+    public Double getProductCost() {
+        return productCost;
+    }
+
+    public void setProductCost(Double productCost) {
+        this.productCost = productCost;
+    }
+
+    public Double getInitialPrice() {
+        return initialPrice;
+    }
+
+    public void setInitialPrice(Double initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+
+
+
+}
